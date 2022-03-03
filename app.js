2d56693b2547ad8d6e95527e2ae401e50177ce86
maldoalex <maldoalex@gmail.com>
2022-03-03 16:11:01 -0600

    JS Filters

---
+++ app.js
@@ -0,0 +1,71 @@
+let filteredProducts = [...products];
+
+const productsContainer = document.querySelector('.products-container');
+
+const displayProducts = () => {
+
+  if (filteredProducts.length < 1) {
+    productsContainer.innerHTML = `<h5>No products matched your search, try a different search.</h5>`;
+    return;
+  }
+
+  productsContainer.innerHTML = filteredProducts.map(({id, title, image, price}) => {
+    return `
+      <article class="product" data-id="${id}">
+        <img src="${image}" class="product-img img">
+        <footer>
+          <h5 class="product-name">${title}</h5>
+          <span class="product-price">$${price}</span>
+        </footer>
+      </article>
+    `;
+  })
+  .join('');
+};
+
+displayProducts();
+
+//Text Filter
+
+const form = document.querySelector('.input-form');
+const searchInput = document.querySelector('.search-input');
+
+form.addEventListener('keyup', () => {
+  const inputValue = searchInput.value;
+  filteredProducts = products.filter(product => {
+    return product.title.toLowerCase().includes(inputValue);
+  });
+  displayProducts();
+});
+
+//Filter Buttons
+
+const companiesDOM = document.querySelector('.companies');
+
+const displayButtons = () => {
+  const buttons = ['all', ...new Set(products.map(product => product.company))];
+
+  companiesDOM.innerHTML = buttons.map(company => {
+    return `
+      <button class="company-btn" data-id="${company}">${company}</button>
+    `;
+  }).join('');
+};
+
+displayButtons();
+
+companiesDOM.addEventListener('click', e => {
+  const el = e.target;
+  if (el.classList.contains('company-btn')){
+    if (el.dataset.id === 'all') {
+      filteredProducts = [...products];
+    }
+    else {
+      filteredProducts = products.filter(product => {
+        return product.company === el.dataset.id;
+      });
+    }
+    searchInput.value = '';
+    displayProducts();
+  }
+});
